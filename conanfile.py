fd260f4667af9b4dbd2716a387ef5935557babbc
gyubong_park <gyubong_park@tmaxsoft.co.kr>
2024-11-29 16:11:42 +0900

    commit

--- conanfile.py
+++ conanfile.py
@@ -2,19 +2,19 @@
 # To keep your changes, remove these comment lines, but the plugin won't be able to modify your requirements
 
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout, CMakeToolchain
+from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps
 
 class ConanApplication(ConanFile):
     package_type = "application"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
-
     tool_requires = "protobuf/3.17.1"
 
     def layout(self):
         cmake_layout(self)
 
     def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
         tc = CMakeToolchain(self)
         tc.user_presets_path = False
         tc.generate()
